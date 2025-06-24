3f4bd2b59c586724d7f789a4b9e665174d4eb33e
MrFrangipane <contact@frangipane.me>
2025-06-24 23:19:37 +0200

    wip(BoardList): prepare reboot/firmware/points buttons

--- ledboarddesktop/board_list/listed_board_widget.py
+++ ledboarddesktop/board_list/listed_board_widget.py
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QWidget, QLabel, QGridLayout
+from PySide6.QtWidgets import QWidget, QLabel, QGridLayout, QPushButton
 
 from ledboardlib import ListedBoard
+from pyside6helpers import icons
 
 
 class ListedBoardWidget(QWidget):
@@ -9,17 +10,50 @@ class ListedBoardWidget(QWidget):
         super().__init__(parent)
 
         self.board = board
-        self.label = QLabel()
 
         layout = QGridLayout(self)
-        layout.addWidget(self.label)
+
+        self.label = QLabel()
+        layout.addWidget(self.label, 0, 0)
+        layout.setColumnStretch(0, 1)
+
+        self.button_reboot_to_bootloader = QPushButton()
+        self.button_reboot_to_bootloader.setToolTip("Reboot")
+        self.button_reboot_to_bootloader.setIcon(icons.refresh())
+        self.button_reboot_to_bootloader.clicked.connect(self._reboot)
+        layout.addWidget(self.button_reboot_to_bootloader, 0, 1)
+
+        self.button_upload_firmware = QPushButton()
+        self.button_upload_firmware.setToolTip("Upload firmware")
+        self.button_upload_firmware.setIcon(icons.upload())
+        self.button_upload_firmware.clicked.connect(self._upload_firmware)
+        layout.addWidget(self.button_upload_firmware, 0, 2)
+
+        self.button_upload_points = QPushButton()
+        self.button_upload_points.setToolTip("Upload sampling points")
+        self.button_upload_points.setIcon(icons.equalizer())
+        self.button_upload_points.clicked.connect(self._upload_points)
+        layout.addWidget(self.button_upload_points, 0, 3)
 
         self.set_board(board)
 
     def set_board(self, board: ListedBoard):
         self.board = board
 
+        self.button_reboot_to_bootloader.setEnabled(self.board.available)
+        self.button_upload_firmware.setEnabled(self.board.available)
+        self.button_upload_points.setEnabled(self.board.available)
+
         if self.board.available:
             self.label.setText(f"{self.board.serial_port_name} - {self.board.hardware_info.name[:-1]}")
         else:
             self.label.setText(f"{self.board.serial_port_name} - Occupied")
+
+    def _reboot(self):
+        print(f"Rebooting: {self.board.serial_port_name}")
+
+    def _upload_firmware(self):
+        print(f"Uploading firmware: {self.board.serial_port_name}")
+
+    def _upload_points(self):
+        print(f"Uploading points: {self.board.serial_port_name}")
