a8d420f22521bd7b690a4ae51fab4e8ce086bdf4
MrFrangipane <contact@frangipane.me>
2025-10-22 17:05:34 +0200

    feat(ControlParameters): restore from InteropData button

--- ledboarddesktop/control_parameters/widget.py
+++ ledboarddesktop/control_parameters/widget.py
@@ -3,8 +3,9 @@ import os.path
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QWidget, QLabel, QGridLayout, QPushButton, QScrollArea
 
-from pyside6helpers import icons
+from pyside6helpers import icons, resources
 from pyside6helpers.annotated_form import AnnotatedFormWidget
+from pyside6helpers.message_box import confirmation_box
 
 from ledboardlib import ListedBoard, ControlParameters, InteropDataStore
 
@@ -14,6 +15,8 @@ from ledboarddesktop.control_parameters.widget_maker import make_control_paramet
 
 
 class ControlParametersWidget(QWidget):
+
+    _interop_filepath = "C:/Users/Ourson/PROJETS/ledboard/ledboard-translator-emulator/ledboardtranslatoremulator/resources/interop-data-melinerion.json"
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -34,15 +37,19 @@ class ControlParametersWidget(QWidget):
         self._button_save_to_emulator.setIcon(icons.laptop())
         self._button_save_to_emulator.clicked.connect(self._save_to_emulator_defaults)
 
+        self._button_restore_from_emulator = QPushButton("Restore from emulator default values")
+        self._button_restore_from_emulator.setIcon(icons.login())
+        self._button_restore_from_emulator.clicked.connect(self._restore_defaults_from_emulator)
         self._scroll_area = QScrollArea()
         self._scroll_area.setWidgetResizable(True)
 
         self._layout = QGridLayout(self)
         self._layout.setContentsMargins(0, 0, 0, 0)
-        self._layout.addWidget(self._label, 0, 0, 1, 2)
-        self._layout.addWidget(self._scroll_area, 1, 0, 1, 2)
+        self._layout.addWidget(self._label, 0, 0, 1, 3)
+        self._layout.addWidget(self._scroll_area, 1, 0, 1, 3)
         self._layout.addWidget(self._button_save_to_board_defaults, 2, 0)
         self._layout.addWidget(self._button_save_to_emulator, 2, 1)
+        self._layout.addWidget(self._button_restore_from_emulator, 2, 2)
 
         Components().board_communicator.boardControlParametersAcquired.connect(self.control_parameters_acquired)
 
@@ -67,11 +74,28 @@ class ControlParametersWidget(QWidget):
         if self._form is None:
             return
 
-        interop_store = InteropDataStore(
-            "C:/Users/Ourson/PROJETS/ledboard/ledboard-translator-emulator/ledboardtranslatoremulator/resources/interop-data-melinerion.json"
-        )
+        interop_store = InteropDataStore(self._interop_filepath)
         interop_store.data.default_control_parameters = self._form.value()
         interop_store.save()
+
+    def _restore_defaults_from_emulator(self):
+        if self._form is None:
+            return
+
+        if not confirmation_box([
+            'This will load default values stored in emulator data.',
+            'Make sure to click "Save as board default values" if you want to save them to the actual board.',
+            'Do you want to continue ?'
+        ]):
+            return
+
+
+        interop_store = InteropDataStore(self._interop_filepath)
+        interop_data = interop_store.data
+        if interop_data.default_control_parameters is None:
+            self._label.setText("No default values stored in emulator data")
+        else:
+            self.control_parameters_acquired(interop_data.default_control_parameters)
 
     @Slot(ControlParameters)
     def control_parameters_acquired(self, parameters: ControlParameters):
